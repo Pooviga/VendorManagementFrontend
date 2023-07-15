67dee1a1d91e0d4b5b8478d5b309072daac48140
MaheshRMR <rameshandramesh11@gmail.com>
2023-07-15 20:13:55 +0530

    view and edit vendor

--- src/component/Viewvendors/Viewvendors.js
+++ src/component/Viewvendors/Viewvendors.js
@@ -6,10 +6,56 @@ import Addvendors from '../Addvendors/Addvendors'
 import 'reactjs-popup/dist/index.css';
 import Popup from 'reactjs-popup'
 import { useEffect, useState } from "react"
+import axios from 'axios'
 
 
 function Viewvendors() {
     const navigate = useNavigate("/")
+    const [vendorDetails, setVendorDetails] = useState([]);
+    const [productDetails, setProductDetails] = useState([]);
+  
+  
+    const [details, setDetails] = useState([]);
+    var data = [];
+    var vdata = [];
+    var pdata = [];
+    // const navigate = useNavigate("/");
+  
+    useEffect(() => {
+      axios.get("https://localhost:7017/api/VendorDetails").then((response) => {
+        setDetails(response.data);
+        response.data.map((newdata) => {
+          vdata.push(newdata.vendorDetails);
+          setVendorDetails(vdata);
+          pdata.push(newdata.productDetails);
+          setProductDetails(pdata);
+        });
+  
+        console.log("asigned", vdata);
+        console.log("asigned", pdata);
+  
+        // console.log('here:',response.data.vendorDetails);
+        // setVendorDetails(response.data.vendorDetails)
+      });
+    }, []);
+  
+    useEffect(() => {
+      console.log("new", details);
+    }, []);
+  
+    function deleteVendor(id) {
+      axios.delete("https://localhost:7017/api/VendorDetails/"+id)
+      .then((response) => {
+          console.log(response.data)
+  
+          })
+      
+          }
+  
+    console.log("asigned 1 :", vendorDetails);
+    console.log("asigned 2 :", productDetails);
+    
+    
     return (
         <div class="viewvendorswholediv">
             <div className="topics">
@@ -50,6 +96,47 @@ function Viewvendors() {
                     </tr>
                 </table>
             </div> */}
+            
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Vendor Name</th>
+                <th scope="col">Type</th>
+                <th scope="col">Status</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {vendorDetails.map((x,index) => {
+                return (
+                  <tr>
+                    <th scope="row">{index+1}</th>
+                    <td>{x.vendorName}</td>
+                    <td>Product</td>
+                    <td>{x.isActive?'active':'inactive'}</td>
+                    <td>
+                      <button style={{fontSize:'24px'}} type="button" class="btn-btn">
+                        <i class="far fa-eye"></i>
+                      </button>
+                      <button style={{fontSize:'24px'}} type="button" class="btn-btn">
+                        <i class="fas fa-edit"></i>
+                      </button>
+                      <button style={{fontSize:'24px'}} type="button" class="btn-btn" onClick={()=>deleteVendor(x.id)}>
+                        <i class="far fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
 
         </div>
     )
