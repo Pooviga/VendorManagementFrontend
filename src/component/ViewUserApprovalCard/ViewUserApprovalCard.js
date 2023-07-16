aabcffa875f1f3c6b452e963f53b63fc0515303d
Github Pooviga <Github spooviga01@gmail.com>
2023-07-16 22:52:39 +0530

    added search and filter in users

--- src/component/ViewUserApprovalCard/ViewUserApprovalCard.js
+++ src/component/ViewUserApprovalCard/ViewUserApprovalCard.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import './ViewUserApprovalCard.css'
+import React from 'react';
+import './ViewUserApprovalCard.css';
 import 'font-awesome/css/font-awesome.min.css';
-import Popup from 'reactjs-popup'
+import Popup from 'reactjs-popup';
 
 const ViewUserApprovalCard = (props) => {
     const status = props.data.approvalStatus;
-    if (status === "Pending") {
 
+    const handleApprove = () => {
+        const apiEndpoint = `https://localhost:7017/api/User/${props.data.id}/4/Approved`;
+        // Replace 'approverId' with the actual ID of the user who is approving the request.
+        // You need to specify the appropriate approver ID here.
+
+        fetch(apiEndpoint, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(props.data),
+        })
+            .then((response) => {
+                // Handle the response from the server if needed
+                alert("approved successfully");
+                console.log('User approved successfully');
+            })
+            .catch((error) => {
+                // Handle errors if the request fails
+                console.error('Error while approving user:', error);
+            });
+    };
+
+    if (status === 'Pending') {
         return (
             <div className="Card">
                 <p><b>Id</b> : {props.data.role.id}</p>
@@ -16,13 +39,47 @@ const ViewUserApprovalCard = (props) => {
                 <p><b>Status</b> : {status}</p>
                 <p><b>Phone Number</b> : {props.data.phoneNumber}</p>
                 <div className="buttons">
-                    <button className="btn">Approve</button>
+                    <button className="btn" onClick={handleApprove}>
+                        Approve
+                    </button>
                     <button className="btn">Decline</button>
                 </div>
-
             </div>
-        )
+        );
+    } else {
+        return null; // Return null if the status is not "Pending"
     }
-}
+};
 
-export default ViewUserApprovalCard
+export default ViewUserApprovalCard;
+
+
+
+// import React from 'react'
+// import './ViewUserApprovalCard.css'
+// import 'font-awesome/css/font-awesome.min.css';
+// import Popup from 'reactjs-popup'
+
+// const ViewUserApprovalCard = (props) => {
+//     const status = props.data.approvalStatus;
+//     if (status === "Pending") {
+
+//         return (
+//             <div className="Card">
+//                 <p><b>Id</b> : {props.data.role.id}</p>
+//                 <p><b>Name</b> : {props.data.name}</p>
+//                 <p><b>Role</b> : {props.data.role.name}</p>
+//                 <p><b>Mail Id</b> : {props.data.email}</p>
+//                 <p><b>Status</b> : {status}</p>
+//                 <p><b>Phone Number</b> : {props.data.phoneNumber}</p>
+//                 <div className="buttons">
+//                     <button className="btn">Approve</button>
+//                     <button className="btn">Decline</button>
+//                 </div>
+
+//             </div>
+//         )
+//     }
+// }
+
+// export default ViewUserApprovalCard
