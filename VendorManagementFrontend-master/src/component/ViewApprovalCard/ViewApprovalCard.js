439cc5f6942948af790ce87d413878ae3b14f23a
Keerthana0505 <keerthanajothi2002@gmail.com>
2023-07-17 17:52:32 +0530

    changes all

---
+++ VendorManagementFrontend-master/src/component/ViewApprovalCard/ViewApprovalCard.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react'
+import './ViewApprovalCard.css'
+import 'font-awesome/css/font-awesome.min.css';
+import Popup from 'reactjs-popup'
+
+
+
+const ViewApprovalCard = (props) => {
+    const status = props.data.purchaseOrderWithUsersName.status;
+    console.log(props.data?.purchaseProducts)
+    if (status === "Pending") {
+        return (
+
+            <div className="Card">
+                <div className='viewdetails'>
+                    <p><b>Purchased Order Id</b> : {props.data.purchaseOrderWithUsersName.id}</p>
+                    {/* <button style={{ fontSize: '24px' }} type="button" class="btn btn-primary"> */}
+                    <Popup className='popmodal' trigger=
+                        {<button onClick={() => { }}>View</button>}
+                        modal nested>
+                        {
+                            close => (
+                                <div className='modal'>
+                                    <div>
+                                        <button className="close" onClick=
+                                            {() => close()}>
+                                            X
+                                        </button>
+                                    </div>
+                                    <div className='content'>
+                                        <p><b>Purchase Order Id </b> : </p>
+                                        <p>{props.data.purchaseOrderWithUsersName.id}</p>
+                                        <p><b>Vendor Name</b> : {props.data.vendorForPurchaseOrder.vendorName}</p>
+                                        <>
+                                            {props.data?.purchaseProducts?.map((products) => (
+
+                                                <div>
+                                                    <p><b>Purchased Products :</b>({products.quantity})</p>
+                                                    <li><b>Product Name</b> : {products.productName} Rs.{products.price}</li>
+                                                    <li><b>Total {products.productName} purchased</b> : {products.quantity}</li>
+                                                    <br></br>
+                                                </div>
+                                            ))}
+                                        </>
+                                        <p><b>Total Amount</b> : Rs.{props.data.purchaseOrderWithUsersName.total}</p>
+                                        <p><b>Ordered On</b> : {props.data.purchaseOrderWithUsersName.orderDateTime}</p>
+
+                                        <p><b>Billing Adress</b> : </p>
+                                        <p>{props.data.purchaseOrderWithUsersName.billingAddress}, {props.data.purchaseOrderWithUsersName.billingAddressCity}</p>
+                                        <p>{props.data.purchaseOrderWithUsersName.billingAddressState}, {props.data.purchaseOrderWithUsersName.billingAddressCountry} </p>
+                                        <p>{props.data.purchaseOrderWithUsersName.billingAddressZipcode}</p>
+                                        <p><b>Shipping Adress </b> : </p>
+                                        <p>{props.data.purchaseOrderWithUsersName.shippingAddress}, {props.data.purchaseOrderWithUsersName.shippingAddressCity}</p>
+                                        <p>{props.data.purchaseOrderWithUsersName.shippingAddressState}, {props.data.purchaseOrderWithUsersName.shippingAddressCountry} </p>
+                                        <p>{props.data.purchaseOrderWithUsersName.shippingAddressZipcode}</p>
+                                        <p><b>Terms and Conditions </b> : {props.data.purchaseOrderWithUsersName.termsAndConditions}</p>
+
+
+                                    </div>
+
+                                </div>
+                            )
+                        }
+                    </Popup>
+                </div>
+
+                {/* <i class="fa-thin fa-circle-info"></i> */}
+                {/* </button> */}
+                <p><b>Status</b> : {status}</p>
+                <p><b>Ordered Date</b> : {props.data.purchaseOrderWithUsersName.orderDateTime}</p>
+                <p><b>Vendor Name</b> : {props.data.vendorForPurchaseOrder.vendorName}</p>
+                <p><b>Purchased Products :</b></p>
+                <>
+                    {props.data?.purchaseProducts?.map((products) => (
+
+                        <div>
+                            <li><b>Product Name</b> : {products.productName}</li>
+                            <li><b>Quantity</b> : {products.quantity}</li>
+                            <br></br>
+                        </div>
+                    ))}
+                </>
+                <div className="buttons">
+                    <button className="btn">Approve</button>
+                    <button className="btn">Decline</button>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+
+export default ViewApprovalCard
---
+++ VendorManagementFrontend-master/src/component/ViewPurchaseOrder/ViewPurchaseOrder.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { FaAlignRight } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+import '../Viewvendors/Viewvendors.css'
+import AddProductPurchaseDetails from '../AddProductPurchaseDetails/AddProductPurchaseDetails'
+import 'reactjs-popup/dist/index.css';
+import Popup from 'reactjs-popup'
+import { useEffect, useState } from "react"
+
+
+function ViewPurchaseOrder() {
+    const navigate = useNavigate("/")
+    return (
+        <div class="viewvendorswholediv">
+            <div className="topics">
+                <h2 className="rh2">Purchased Product Details</h2>
+                <Popup trigger=
+                    {<button className="add_button" onClick={() => { navigate('/addvendor') }}>Create Purchase Order</button>}
+                    modal nested>
+                    {
+                        close => (
+                            <div className='modal'>
+                                <div>
+                                    <button className="close" onClick=
+                                        {() => close()}>
+                                        X
+                                    </button>
+                                </div>
+                                <div className='content'>
+                                    <AddProductPurchaseDetails />
+                                </div>
+
+                            </div>
+                        )
+                    }
+                </Popup>
+                {/* <button className="add_button" onClick={() => { navigate('/addvendor') }}>Add Vendor</button> */}
+            </div>
+            {/* <div class="table_overflow">
+                <table id="vendorTable">
+                    <tr>
+                        <th>Vendor Name</th>
+                        <th>Vendor Type</th>
+                        <th>Contact</th>
+                        <th>Vendor Email</th>
+                        <th>Action</th>
+                    </tr>
+                </table>
+            </div> */}
+
+        </div>
+    )
+}
+
+export default ViewPurchaseOrder
